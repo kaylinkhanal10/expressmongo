159e9d62d46ced056b1ef3f0c2eea062b105c398
Kyalima <kyalima@Kyalima.local>
2024-01-11 07:22:14 +0545

    add crud routes

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -11,7 +11,24 @@ const getAllUsers  = async(req, res)=> {
     res.json({users})
 }
 
+const getUserById  = async(req, res)=> {
+  const userDetails =  await User.findById(req.params.id)
+  res.json({userDetails})
+}
 
-
-
-module.exports = {registerNewUser,getAllUsers}
+const deleteUserById  = async(req, res)=> {
+    const result =  await User.findByIdAndDelete(req.params.id)
+    if(result) {
+        res.json({msg: "deleted successfully"})
+    }
+  }
+  
+  const updateUserDetailsById  = async(req, res)=> {
+    const result =  await User.findByIdAndUpdate(req.params.id, req.body)
+    if(result) {
+        res.json({msg: "updated successfully"})
+    }
+  }
+  
+  
+module.exports = {registerNewUser,getAllUsers, getUserById,deleteUserById,updateUserDetailsById}
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const {registerNewUser,getAllUsers} = require('../controllers/users')
+const {registerNewUser,getAllUsers,getUserById, deleteUserById,updateUserDetailsById} = require('../controllers/users')
 
 router = express.Router();
 
 router.post("/register", registerNewUser);
 router.get("/users", getAllUsers);
+router.get("/users/:id", getUserById);
+router.delete("/users/:id", deleteUserById);
+router.put("/users/:id", updateUserDetailsById);
 
 module.exports = router
